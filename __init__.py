9e9313b964a4e5c032b2109321aedc582bdcc9f0
Ricardo Daniel Garibaldi Oliva <garibaldi.rdaniel@gmail.com>
2025-07-25 22:45:14 -0500

    Better point entity handling

--- __init__.py
+++ __init__.py
@@ -24,6 +24,7 @@ from pathlib import Path
 from bpy_extras import object_utils
 import mathutils
 import os
+import math
 
 
 class ImportRMeshOperator(bpy.types.Operator, ImportHelper):
@@ -34,9 +35,32 @@ class ImportRMeshOperator(bpy.types.Operator, ImportHelper):
     # Add import options as properties
 
     use_imp_mats: bpy.props.BoolProperty(name="Import Materials", default=True)
-    use_ents: bpy.props.BoolProperty(name="Import Entities as empties", default=True)
-    use_lightmaps: bpy.props.BoolProperty(name="Import Lightmaps", default=True)
-    use_ue: bpy.props.BoolProperty(name="Ultimate Edition Compatibility", default=True)
+    use_ents: bpy.props.BoolProperty(name="Import Entities", 
+                                     description="Imports point entities as Empties.",
+                                     default=True)
+    use_lights_as_ents: bpy.props.BoolProperty(name="Use Blender Lights", 
+                                               description="Imports light entities as Blender Lights instead of Empties.",
+                                               default=False
+                                               )
+    use_mesh_as_name: bpy.props.BoolProperty(name="Mesh name as object name", 
+                                             description="Sets the object name to the mesh name for model/mesh entities",
+                                             default=False)
+    use_lightmaps: bpy.props.BoolProperty(name="Import Lightmaps", default=False)
+    use_ue: bpy.props.BoolProperty(name="UE Compatibility", 
+                                   description="Accounts for Ultimate Edition RMESH files, which have a different slot for lightmaps and search for textures in a 'textures' folder.",
+                                   default=False)
+    
+    base_scale: bpy.props.FloatProperty(
+        name="Base Scale",
+        description="(0.0075 for CB:HDEdition), (0.00390625 for Blitz3D CB)",
+        default=0.0075,
+        min=0.00390625,
+        max=1.0,
+        soft_min=0.00390625,
+        soft_max=1.0,
+        step=0.0005,
+        precision=10
+    )
 
     filename_ext = ".rmesh"
     filter_glob: bpy.props.StringProperty(default="*.rmesh", options={'HIDDEN'})
@@ -177,7 +201,8 @@ class ImportRMeshOperator(bpy.types.Operator, ImportHelper):
 
             for i in range(rmesh.submeshes[submesh].vertex_count):
                 vert = rmesh.submeshes[submesh].vertices[i].position
-                bm.verts.new((vert.x,vert.z,vert.y))
+                scaled_pos = (vert.x * self.base_scale, vert.z * self.base_scale, vert.y * self.base_scale)
+                bm.verts.new(scaled_pos)
 
             bm.verts.ensure_lookup_table()
             bm.verts.index_update()
@@ -186,7 +211,6 @@ class ImportRMeshOperator(bpy.types.Operator, ImportHelper):
 
             for i in range(rmesh.submeshes[submesh].index_count):
                 idxs = rmesh.submeshes[submesh].indices[i]
-                # bm.faces.new([bm.verts[idxs.z],bm.verts[idxs.y],bm.verts[idxs.x]])
                 try:
                     bm.faces.new([bm.verts[idxs.z],bm.verts[idxs.y],bm.verts[idxs.x]])
                     added_idxs = added_idxs+1
@@ -224,16 +248,177 @@ class ImportRMeshOperator(bpy.types.Operator, ImportHelper):
             if (self.use_imp_mats):
                 new_object.data.materials.append(mats[mat_name])
 
+        # Import invisible meshes here.
+        # Todo -> import invisible meshes.
+
+        # Import entities
         if(self.use_ents):
             ent_parent = object_utils.object_data_add(context, None, name="entities")
             ent_parent.parent = obj_parent
+            
             for i in range(rmesh.point_count):
-                ent_name = str(i) + "_" + rmesh.entities[i].entity_type.value
-                if(rmesh.entities[i].entity_type.value == u"model"):
-                    ent_name = ent_name + "_" + rmesh.entities[i].entity.model_name.value
-                ent = object_utils.object_data_add(context, None, name=ent_name)
-                ent.location = (rmesh.entities[i].entity.position.x, rmesh.entities[i].entity.position.z, rmesh.entities[i].entity.position.y)
+                entity = rmesh.entities[i]
+                entity_type = entity.entity_type.value
+                entity_data = entity.entity
+                
+
+                ent_name = f"{i}_{entity_type}"
+                
+                if entity_type in ["light", "light_fix", "spotlight"] and self.use_lights_as_ents:
+                    # Create actual Blender light objects
+                    if entity_type == "spotlight":
+                        light_data = bpy.data.lights.new(name=f"{ent_name}_data", type='SPOT')
+                        
+                        # Set spotlight properties
+                        light_data.energy = entity_data.intensity * 100  # Scale for Blender
+                        light_data.spot_size = math.radians(entity_data.outter_cone_angle)
+                        light_data.spot_blend = (entity_data.outter_cone_angle - entity_data.inner_cone_angle) / entity_data.outter_cone_angle
+                        light_data.use_shadow = False
+
+
+                        # Parse color string (assuming format like "255 128 64" or hex)
+                        try:
+                            color_str = entity_data.color.value
+                            if color_str.startswith('#'):
+                                # Hex color
+                                color_val = int(color_str[1:], 16)
+                                r = ((color_val >> 16) & 255) / 255.0
+                                g = ((color_val >> 8) & 255) / 255.0
+                                b = (color_val & 255) / 255.0
+                            else:
+                                # Space-separated RGB values
+                                rgb = [float(x) / 255.0 for x in color_str.split()]
+                                r, g, b = rgb[0], rgb[1], rgb[2]
+                            light_data.color = (r, g, b)
+                        except:
+                            print(f"Could not parse spotlight color: {entity_data.color.value}")
+                            light_data.color = (1.0, 1.0, 1.0)
+                        
+                        # Create object
+                        ent = bpy.data.objects.new(ent_name, light_data)
+                        context.collection.objects.link(ent)
+                        
+                        # Set position and rotation
+                        ent.location = (entity_data.position.x * self.base_scale, 
+                                  entity_data.position.z * self.base_scale, 
+                                  entity_data.position.y * self.base_scale)
+                        
+                        # This might crash because i dont rememeber how angles are stored in rmesh files
+                        try:
+                            angles_str = entity_data.angles.value
+                            angles = [math.radians(float(x)) for x in angles_str.split()]
+                            if len(angles) >= 3:
+                                # Convert from game rotation to Blender (may need adjustment)
+                                ent.rotation_euler = (angles[0], angles[2], angles[1])
+                        except:
+                            print(f"Could not parse spotlight angles: {entity_data.angles.value}")
+                        
+                        # Add custom properties for spotlight-specific data
+                        ent["inner_cone_angle"] = entity_data.inner_cone_angle
+                        ent["outer_cone_angle"] = entity_data.outter_cone_angle
+                        ent["range"] = entity_data.range
+                        
+                    else:  # light or light_fix
+                        light_data = bpy.data.lights.new(name=f"{ent_name}_data", type='POINT')
+                        light_data.use_shadow = False
+                        
+                        # Set light properties
+                        if hasattr(entity_data, 'intensity'):
+                            light_data.energy = entity_data.intensity * 100  # Scale for Blender
+                        
+                        # Parse color
+                        try:
+                            color_str = entity_data.color.value
+                            if color_str.startswith('#'):
+                                color_val = int(color_str[1:], 16)
+                                r = ((color_val >> 16) & 255) / 255.0
+                                g = ((color_val >> 8) & 255) / 255.0
+                                b = (color_val & 255) / 255.0
+                            else:
+                                rgb = [float(x) / 255.0 for x in color_str.split()]
+                                r, g, b = rgb[0], rgb[1], rgb[2]
+                            light_data.color = (r, g, b)
+                        except:
+                            print(f"Could not parse light color: {entity_data.color.value}")
+                            light_data.color = (1.0, 1.0, 1.0)
+                        
+                        # Create object
+                        ent = bpy.data.objects.new(ent_name, light_data)
+                        context.collection.objects.link(ent)
+                        ent.location = (entity_data.position.x * self.base_scale, 
+                                  entity_data.position.z * self.base_scale, 
+                                  entity_data.position.y * self.base_scale)
+                        
+                        # Add custom properties
+                        if hasattr(entity_data, 'range'):
+                            ent["range"] = entity_data.range
+                
+                else:
+                    ent = object_utils.object_data_add(context, None, name=ent_name)
+                    
+                    # Set position
+                    
+                    ent.location = (entity_data.position.x * self.base_scale, 
+                                  entity_data.position.z * self.base_scale, 
+                                  entity_data.position.y * self.base_scale)
+                    
+                    # Set rotation if available
+                    if hasattr(entity_data, 'rotation'):
+                        ent.rotation_mode = 'XYZ'
+                        # print(f"Entity #{i} [{entity_type}]: x{entity_data.rotation.x} y{entity_data.rotation.y} z{entity_data.rotation.z}  ")
+                        ent.rotation_euler = (math.radians(entity_data.rotation.x), 
+                                    math.radians(entity_data.rotation.z), 
+                                    math.radians(entity_data.rotation.y))
+
+                    
+                    # Set scale if available
+                    if hasattr(entity_data, 'scale'):
+                        ent.scale = (entity_data.scale.x, entity_data.scale.z, entity_data.scale.y)
+                        #We don't scale the scale, we assume user is providing it scaled by ROOMSCALE
+                    
+                    # Add custom properties based on entity type
+                    if entity_type == "model":
+                        if self.use_mesh_as_name:
+                            ent_name += f"_{entity_data.model_name.value}"
+                        ent.name = ent_name
+                        ent["model_name"] = entity_data.model_name.value
+                        if hasattr(entity_data, 'fx'):
+                            ent["fx"] = entity_data.fx
+                    
+                    elif entity_type == "mesh":
+                        if self.use_mesh_as_name:
+                            ent_name += f"_{entity_data.model_name.value}"
+                        ent.name = ent_name
+                        ent["model_name"] = entity_data.model_name.value
+                        ent["has_collision"] = bool(entity_data.has_collision)
+                        ent["fx"] = entity_data.fx
+                        ent["texture"] = entity_data.texture.value
+                    
+                    elif entity_type == "screen":
+                        ent["screen_texture"] = entity_data.screen_texture.value
+                    
+                    elif entity_type == "save_screen":
+                        ent["model"] = entity_data.model.value
+                        ent["screen_texture"] = entity_data.screen_texture.value
+                    
+                    elif entity_type == "soundemitter":
+                        ent["sound_index"] = entity_data.sound_index
+                        ent["range"] = entity_data.range
+                    
+                    elif entity_type == "waypoint":
+                        # Waypoints only have position, which is already set
+                        pass
+                    
+                    elif entity_type == "playerstart":
+                        try:
+                            angles_str = entity_data.angles.value
+                            ent["angles"] = angles_str
+                        except:
+                            pass
+                
+                # Set parent
                 ent.parent = ent_parent
+                ent["entity_type"] = entity_type
 
         return {'FINISHED'}
 
